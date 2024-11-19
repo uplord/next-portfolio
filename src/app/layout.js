d2deeb932f2f76444f7708d0153bf752ad1970bf
Michael <michael@uplord.co.uk>
2024-11-19 15:20:17 +0000

    dark mode on 404

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,19 @@
 import "../styles/style.scss";
+import { getDarkMode } from '@/utils/getDarkMode';
 
 export default function RootLayout({ children }) {
+  const themeScript = `
+    (${getDarkMode.toString()})();
+  `;
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <script
+          dangerouslySetInnerHTML={{ __html: themeScript }}
+        ></script>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
