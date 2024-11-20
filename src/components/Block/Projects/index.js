de2fab4c6e756b461ac398f3aac02a05880c0ea4
Michael <michael@uplord.co.uk>
2024-11-20 14:06:55 +0000

    header and projects amend

--- src/components/Block/Projects/index.js
+++ src/components/Block/Projects/index.js
@@ -1,70 +1,50 @@
 import styles from "./style.module.scss";
-import Image from 'next/image';
-import Svg from '@/components/Svg';
+import Image from "next/image";
+import Svg from "@/components/Svg";
+
+const projectsData = [
+  { type: "svg", name: "brewdog" },
+  { type: "svg", name: "gdk" },
+  { type: "svg", name: "subway" },
+  { type: "svg", name: "umbro" },
+  { type: "svg", name: "superga" },
+  { type: "svg", name: "dundeeunited" },
+  { type: "svg", name: "hungrrr" },
+  { type: "svg", name: "snappy" },
+  { type: "svg", name: "macdonalds" },
+  { type: "svg", name: "campbells" },
+  { type: "image", src: "/images/petervardy.png", alt: "Peter Vardy" },
+  { type: "image", src: "/images/carmoney.png", alt: "CarMoney" },
+  { type: "svg", name: "bluewater" },
+];
 
 export default function Projects() {
   return (
     <div id="projects" className={styles.projectsSection}>
-      <div className={`container ${styles.container || ''}`}>
+      <div className={`container ${styles.container}`}>
         <div className={styles.grid}>
           <div className={styles.text}>
             <h2>Projects I've worked on</h2>
           </div>
           <div className={styles.projects}>
-            <div className={styles.project}>
-              <Svg name="brewdog" width={900} height={600} />
-            </div>
-            <div className={styles.project}>
-              <Svg name="gdk" width={900} height={600} />
-            </div>
-            <div className={styles.project}>
-              <Svg name="subway" width={900} height={600} />
-            </div>
-            <div className={styles.project}>
-              <Svg name="umbro" width={900} height={600} />
-            </div>
-            <div className={styles.project}>
-              <Svg name="superga" width={900} height={600} />
-            </div>
-            <div className={styles.project}>
-              <Svg name="dundeeunited" width={900} height={600} />
-            </div>
-            <div className={styles.project}>
-              <Svg name="hungrrr" width={900} height={600} />
-            </div>
-            <div className={styles.project}>
-              <Svg name="snappy" width={900} height={600} />
-            </div>
-            <div className={styles.project}>
-              <Svg name="macdonalds" width={900} height={600} />
-            </div>
-            <div className={styles.project}>
-              <Svg name="campbells" width={900} height={600} />
-            </div>
-            <div className={styles.project}>
-              <Image
-                src="/images/petervardy.png"
-                alt="Peter Vardy"
-                quality={80}
-                width={900}
-                height={600}
-              />
-            </div>
-            <div className={styles.project}>
-              <Image
-                src="/images/carmoney.png"
-                alt="CarMoney"
-                quality={80}
-                width={900}
-                height={600}
-              />
-            </div>
-            <div className={styles.project}>
-              <Svg name="bluewater" width={900} height={600} />
-            </div>
+            {projectsData.map((project, index) => (
+              <div key={index} className={styles.project}>
+                {project.type === "svg" ? (
+                  <Svg name={project.name} width={900} height={600} />
+                ) : (
+                  <Image
+                    src={project.src}
+                    alt={project.alt}
+                    quality={80}
+                    width={900}
+                    height={600}
+                  />
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
