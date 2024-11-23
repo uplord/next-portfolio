0f84e3b0cbb6b8c90975a558137fa9bf2038e8a4
Michael <michael@uplord.co.uk>
2024-11-23 19:58:12 +0000

    social change

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -6,14 +6,14 @@ import Svg from '@/components/Svg';
 export default function Social() {
   return (
     <div className={styles.social}>
-      <Link href="https://www.linkedin.com/in/themichael/" target="_blank">
-        <Svg name="linkedin-in-brands-solid" width={20} height={20} color="#0077b5" />
+      <Link href="https://www.linkedin.com/in/themichael/" target="_blank" className={styles.linkedin}>
+        <Svg name="linkedin-in-brands-solid" width={20} height={20} color="#fff" />
       </Link>
-      <Link href="https://www.instagram.com/michael.adam.allen/" target="_blank">
-        <Svg name="instagram-brands-solid" width={20} height={20} color="#d62976" />
+      <Link href="https://www.instagram.com/michael.adam.allen/" target="_blank" className={styles.instagram}>
+        <Svg name="instagram-brands-solid" width={20} height={20} color="#fff" />
       </Link>
       <Link href="mailto:michael@uplord.co.uk">
-        <Svg name="envelope-solid" width={20} height={20} color="var(--primary-color)" />
+        <Svg name="envelope-solid" width={20} height={20} color="#fff" />
       </Link>
     </div>
   )
